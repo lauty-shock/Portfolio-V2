f61509bb4b086cd36ee297f8f3ee9cba9a9f8552
lauty-shock <lautaroperez135@gmail.com>
2022-12-01 01:45:57 -0300

    start of the project section

--- src/components/Burger/BurgerMenu/BurgerMenu.jsx
+++ src/components/Burger/BurgerMenu/BurgerMenu.jsx
@@ -7,53 +7,64 @@ import { FaUserGraduate } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 import css from "./BurgerMenu.module.css";
+import { useState } from "react";
+import Contac from "../../Contac/Contac";
 
 export default function BurgerMenu() {
   const lenguage = useSelector((state) => state.lenguage);
   const mode = useSelector((state) => state.mode);
+  const [viewContac, setViewContac] = useState(false);
+
+  function seeContac() {
+    setViewContac(!viewContac);
+  }
   return (
-    <div className={css.menuNavbar}>
-      <Link
-        to="/"
-        className={
-          mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
-        }
-        onClick={(e) => setCollapsed(!collapsed)}
-      >
-        <FaHome /> {lenguage === "ESP" ? "Inicio" : "Home"}
-      </Link>
-      <Link
-        to="/about"
-        className={
-          mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
-        }
-        onClick={(e) => setCollapsed(!collapsed)}
-      >
-        <FaUserGraduate /> {lenguage === "ESP" ? "Sobre mi" : "About me"}
-      </Link>
-      <Link
-        to="/projects"
-        className={
-          mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
-        }
-        onClick={(e) => setCollapsed(!collapsed)}
-      >
-        <FaLayerGroup /> {lenguage === "ESP" ? "Proyectos" : "Projects"}
-      </Link>
-      <span
-        className={
-          mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
-        }
-      >
-        <MdDescription /> {lenguage === "ESP" ? "CV" : "Resume"}
-      </span>
-      <span
-        className={
-          mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
-        }
-      >
-        <MdEmail /> {lenguage === "ESP" ? "Contacto" : "Contact"}
-      </span>
-    </div>
+    <>
+      <div className={css.menuNavbar}>
+        <Link
+          to="/"
+          className={
+            mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
+          }
+          onClick={(e) => setCollapsed(!collapsed)}
+        >
+          <FaHome /> {lenguage === "ESP" ? "Inicio" : "Home"}
+        </Link>
+        <Link
+          to="/about"
+          className={
+            mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
+          }
+          onClick={(e) => setCollapsed(!collapsed)}
+        >
+          <FaUserGraduate /> {lenguage === "ESP" ? "Sobre mi" : "About me"}
+        </Link>
+        <Link
+          to="/projects"
+          className={
+            mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
+          }
+          onClick={(e) => setCollapsed(!collapsed)}
+        >
+          <FaLayerGroup /> {lenguage === "ESP" ? "Proyectos" : "Projects"}
+        </Link>
+        <li
+          className={
+            mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
+          }
+        >
+          <MdDescription /> {lenguage === "ESP" ? "CV" : "Resume"}
+        </li>
+        <li
+          className={
+            mode === "dark" ? `${css.seccionDark}` : `${css.seccionLight}`
+          }
+          onClick={seeContac}
+        >
+          <MdEmail /> {lenguage === "ESP" ? "Contacto" : "Contact"}
+        </li>
+      </div>
+      {viewContac && <Contac handleSee={seeContac} />}
+    </>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,10 +1,16 @@
-import React from "react";
 import css from "./Projects.module.css";
 
 export default function Projects() {
   return (
-    <center>
-      <h1>Projects</h1>
-    </center>
+    <div className={css.container}>
+      <div className={css.containerProject}>
+        <div className={css.containerTitle}>
+          <h1>#1 Projects</h1>
+        </div>
+        <div className={css.containerImg}>
+          <div className={css.img}></div>
+        </div>
+      </div>
+    </div>
   );
 }
