5e14a045e198ff0009f8442fdec4a1a731a4a617
lauty-shock <lautaroperez135@gmail.com>
2023-03-20 02:55:48 -0300

    Control de errores en contacto (con Formik)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,99 +1,117 @@
 import css from "./Contact.module.css";
-import { useState } from "react";
 import axios from "axios";
 import Swal from "sweetalert2";
+import { useFormik } from "formik";
 
 export default function Contact() {
-  const [input, setInput] = useState({
-    email: "",
-    name: "",
-    mensaje: "",
-  });
-  function handleInput(e) {
-    setInput({
-      ...input,
-      [e.target.name]: e.target.value,
-    });
-  }
   function submit(e) {
     e.preventDefault();
-    if (!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/.test(input.email)) {
-      Swal.fire({
-        // background: "black",
-        border: "2px solid blue",
-        // color: "white",
-        title: "¡Error!",
-        text: "Por favor complete con un email valido",
-        icon: "error",
-        timer: 3000,
-      });
-    } else if (!input.name || input.name.length < 4) {
-      Swal.fire({
-        background: "black",
-        color: "white",
-        title: "¡Error!",
-        text: "Por favor complete con su nombre",
-        icon: "error",
-        timer: 3000,
-      });
-    } else if (input.mensaje.length < 8) {
-      Swal.fire({
-        background: "black",
-        color: "white",
-        title: "¡Error!",
-        text: "Por favor complete con la razón de su comunicado",
-        icon: "error",
-        timer: 3000,
-      });
-    } else {
-      axios.post("https://submit-form.com/nT4t6IoQ", input);
+    axios.post("https://submit-form.com/nT4t6IoQ", formik.values);
+    formik.resetForm()
 
-      setInput({
-        email: "",
-        name: "",
-        mensaje: "",
-      });
-
-      Swal.fire({
-        background: "rgb(100, 100, 100)",
-        color: "white",
-        title: "¡Aprecio su interes!",
-        text: "¡Me comunicaré a la brevedad!",
-        icon: "success",
-        timer: 4000,
-        timerProgressBar: true,
-      });
-    }
+    Swal.fire({
+      background: "rgb(100, 100, 100)",
+      color: "white",
+      title: "¡Aprecio su interes!",
+      text: "¡Me comunicaré a la brevedad!",
+      icon: "success",
+      timer: 4000,
+      timerProgressBar: true,
+    });
   }
+  const validate = (values) => {
+    const errors = {};
+    if (!values.name) {
+      errors.name = "Requerido";
+    } else if (values.name.length < 3) {
+      errors.name = "Su nombre debe superar los 3 caracteres";
+    }
+
+    if (!values.mensaje) {
+      errors.mensaje = "Requerido";
+    } else if (values.mensaje.length < 20) {
+      errors.mensaje = "Su mensaje debe superar los 20 caracteres";
+    }
+
+    if (!values.email) {
+      errors.email = "Requerido";
+    } else if (
+      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+    ) {
+      errors.email = "Correo electrónico invalido";
+    }
+
+    return errors;
+  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      mensaje: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      submit();
+    },
+    resetForm: () => {
+      formik.values = {
+        name: "",
+        email: "",
+        mensaje: "",
+      };
+    },
+  });
+
   return (
     <div id="contacto" className={css.container}>
       <span className={css.title}>CONTACTO</span>
       <div className={css.form}>
-        <label htmlFor="">Nombre</label>
+        <label htmlFor="name">Nombre</label>
         <input
-          type="text"
+          id="name"
           name="name"
-          value={input.name}
-          onChange={(e) => handleInput(e)}
+          type="text"
+          onChange={formik.handleChange}
+          value={formik.values.name}
           placeholder="Tu nombre"
         />
-        <label htmlFor="">Correo electrónico</label>
+        {formik.errors.name ? (
+          <div className={css.error}>{formik.errors.name}</div>
+        ) : null}
+
+        <label htmlFor="email">Correo electrónico</label>
         <input
-          type="email"
+          id="email"
           name="email"
-          value={input.email}
-          onChange={(e) => handleInput(e)}
+          type="email"
+          onChange={formik.handleChange}
+          value={formik.values.email}
           placeholder="ejemplo@gmail.com"
         />
-        <label htmlFor="">Su mensaje</label>
+        {formik.errors.email ? (
+          <div className={css.error}>{formik.errors.email}</div>
+        ) : null}
+
+        <label htmlFor="mensaje">Su mensaje</label>
         <textarea
+          id="mensaje"
           name="mensaje"
-          value={input.mensaje}
-          onChange={(e) => handleInput(e)}
+          onChange={formik.handleChange}
+          value={formik.values.mensaje}
           placeholder="¡Hola! Quiero contactarme con vos porque..."
-        ></textarea>
+        />
+        {formik.errors.mensaje ? (
+          <div className={css.errorTextarea}>{formik.errors.mensaje}</div>
+        ) : null}
+
         <center>
-          <button onClick={(e) => submit(e)}>Enviar mensaje</button>
+          {Object.keys(formik.errors).length !== 0 ? (
+            <button className={css.inavilitado}>Error</button>
+          ) : (
+            <button className={css.avilitado} onClick={submit}>
+              Enviar mensaje
+            </button>
+          )}
         </center>
       </div>
     </div>
